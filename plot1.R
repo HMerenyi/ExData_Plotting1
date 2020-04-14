5412beba9240c9a8604eb701b8670b60cdc495a8
H Merenyi <hanna.merenyi@gmail.com>
2020-04-14 21:45:39 +0200

    plot 1 finished

---
+++ plot1.R
@@ -0,0 +1,13 @@
+#reading in the dataset
+df<-read.delim("household_power_consumption.txt", header = FALSE, sep = ";", skip = 66637, nrows = 2880)
+header<-read.delim("household_power_consumption.txt", sep = ";", nrows = 1)
+names(df)<-names(header)
+
+
+#making plot1
+hist(df$Global_active_power, col = "red", main = "Global Active Power", cex.axis=0.8, 
+     xlab = "Global Active Power (kilowatts)")
+
+#saving to png
+png(filename = "plot1.png")
+dev.off()
